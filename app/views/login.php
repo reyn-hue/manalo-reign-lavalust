c6d31c3eec89d38f16b2eef3735bfdf9cf6ec532
reign <reignmanalo03@gmail.com>
2026-09-14 21:19:29 +0800

    Fix login heading layout

--- app/views/login.php
+++ app/views/login.php
@@ -66,15 +66,17 @@
     </style>
 </head>
 <body>
-    <h1>Login</h1>
-    <form method="POST" action="/authenticate">
-        <label>Username:</label>
-        <input type="text" name="username" required>
-        <br><br>
-        <label>Password:</label>
-        <input type="password" name="password" required>
-        <br><br>
-        <button type="submit">Login</button>
-    </form>
+    <main class="login-container">
+        <h1>Login</h1>
+        <form method="POST" action="/authenticate">
+            <label for="username">Username:</label>
+            <input id="username" type="text" name="username" required>
+
+            <label for="password">Password:</label>
+            <input id="password" type="password" name="password" required>
+
+            <button type="submit">Login</button>
+        </form>
+    </main>
 </body>
 </html>
